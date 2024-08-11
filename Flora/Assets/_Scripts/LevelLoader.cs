e37f62edff8c6ee82632ddb92a146abef6152362
ben <bmfboom9@gmail.com>
2024-08-11 03:14:07 -0600

    created main menu / level select

---
+++ Flora/Assets/_Scripts/LevelLoader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public int levelNumber;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level " +  levelNumber.ToString());
+    }
+
+}
